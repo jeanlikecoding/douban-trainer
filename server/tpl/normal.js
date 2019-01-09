b6d23305271ac69f3524314cd3f8c41bb7d7cb29
DESKTOP-0OE3G69\1 <jeanlikecoding@126.com>
2019-01-09 18:16:05 +0800

    feat: 新增了静态 HTML 模板

---
+++ server/tpl/normal.js
@@ -0,0 +1,27 @@
+module.exports = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Koa server Html</title>
+      <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+      <link href="https://cdn.bootcss.com/twitter-bootstrap/4.1.2/css/bootstrap.css" rel="stylesheet">
+      <script src="https://cdn.bootcss.com/twitter-bootstrap/4.1.2/js/bootstrap.bundle.js"></script>
+      
+      </head>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-6 col-md-8">
+            <h1>Hi Jean</h1>
+            <p>This is Test</p>
+          </div>
+          <div class="col-xs-6 col-md-4">
+            <p>测试 HTML 页面</p>
+          </div>
+        </div>
+      </div>
+    </body>
+  </html>
+  `
